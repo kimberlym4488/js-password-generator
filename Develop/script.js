d1ab8f219c8f57a5bf34c29e492215dceaeabe1e
SamusMoran <kimberlym4488@comcast.net>
2021-11-15 23:21:07 -0800

    Adds categories for pulling from the indexes and combining them into characterSelectionPool

--- Develop/script.js
+++ Develop/script.js
@@ -6,28 +6,51 @@ specialCharacters
 lowercaseLetters
 uppercaseLetters
 numbers*/ 
+var numbers = ["0","1","2"];
+var specialCharacters = ["*", "&", "#"];
+var lowercaseLetters = ["a","c","t"];
 
 /*We have to create prompts and confirm if the user wants:
 When the user clicks the generate button it prompts them to confirm choices:
 Lowercase, uppdercase, numbers, and/or special characters. First step: what happens when the user clicks the generatePassword button?*/
 function generatePassword(){
+  var returnVal = "";
+  var characterSelectionPool = [];
+//user prompt:password length. If invalid, return user to beginning of this segment.
+  var passwordLength = prompt("It's time to generate your password! \nHow long would you like your password to be? \n(Please choose between 8 to 128 characters)")
 
-  var hasSpecialCharacters = confirm("It's time to generate your password! \nWould you like to include special characters? i.e. *,&,#,>");
+  if (passwordLength >=8 &&
+    passwordLength <=128){
+
+    console.log("passwordLength",passwordLength);
+    alert("Your password will be" + passwordLength + ".")}
+
+    else{
+    console.log("Invalid Entry");
+   //alert("Please choose a number between 8 and 128.");
+    return "Please choose a number between 8 and 128"; //This logged in the generate password box!!!
+}
+
+  var hasSpecialCharacters = confirm("Would you like to include special characters? i.e. *,&,#,>");
 //user prompt: special characters
     if (hasSpecialCharacters){
-    console.log("True");
-    alert("You have chosen to include special characters.");}
+    console.log("Has Special Characters: True");
+    characterSelectionPool = characterSelectionPool.concat(specialCharacters);
+  }
+    //alert("You have chosen to include special characters.");}
     
       else{
-      console.log("False");
-      alert("NOTE: You have chose to NOT include special characters. \nYour password is stronger WITH special characters.");
+      console.log("Has Special Characters:False");
+      //alert("NOTE: You have chose to NOT include special characters. \nYour password is stronger WITH special characters.");
 }
 //user prompt:lowercase letters
   var hasLowercaseLetters = confirm("Would you like to include lowercase letters? i.e. a,d,f,g");
 
     if (hasLowercaseLetters){
     console.log("True");
-    alert("You have chosen to include lowercase letters.")}
+    characterSelectionPool = characterSelectionPool.concat(lowercaseLetters);
+    }
+//alert("You have chosen to include lowercase letters.")}
   
       else{
       console.log("False");
@@ -50,36 +73,38 @@ function generatePassword(){
 
     if (hasNumbers){
     console.log("True");
-    alert("You have chosen to include numbers.")}
+    characterSelectionPool=characterSelectionPool.concat(numbers);
+    }
+
+    // alert("You have chosen to include numbers.")}
 
       else{
       console.log("False");
       alert("You have chosen NOT to include numbers.");
+      }
 
-  //user entry: password length?
+    if (characterSelectionPool.length > 0){
+      for(i=0; i < passwordLength.length; i++) {
+        returnVal = returnVal + characterSelectionPool[
+          Math.floor((Math.random)*characterSelectionPool.length)]
+          console.log(returnVal);
 
-  var passwordLength = confirm("Would you like to include numbers? i.e. 5,8,3");
 
-  if (hasNumbers){
-  console.log("True");
-  alert("You have chosen to include numbers.")}
+    }  
+return returnVal;
+    }
+  }
 
-    else{
-    console.log("False");
-    alert("You have chosen NOT to include numbers.");
-      
-}
-}
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword(){
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
-}
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
