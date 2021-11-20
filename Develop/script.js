2c4d18eaeaed8f9a95b282ff0aec807f0983aa3b
SamusMoran <kimberlym4488@comcast.net>
2021-11-19 17:22:19 -0800

    Adds more context but now the passwords keep appending unless screen is refreshed

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,7 @@ var specialCharacters = ["*", "&", "#", "%", "@"];
 var lowercaseLetters = ["a", "c", "t", "b", "e", "f"];
 var uppercaseLetters = ["A", "B", "D", "F", "H", "J", "K", "L"]
 
+
 /*We have to create prompts and confirm if the user wants:
 When the user clicks the generate button it prompts them to confirm choices:
 Lowercase, uppdercase, numbers, and/or special characters. First step: what happens when the user clicks the generatePassword button?*/
@@ -19,18 +20,21 @@ var passwordText = "";
 var characterSelectionPool = [];
 
 function generatePassword() {
+  passwordText.value = "#password";
 
   //user prompt:password length. If invalid,the return at the end of password length if statement will bring the user to beginning of this segment again.
-  var passwordLength = prompt("It's time to generate your password! \nHow long would you like your password to be? \n(Please choose between 8 to 128 characters)")
-  passwordLength = parseInt(passwordLength);
- 
+  var passwordLength =prompt("It's time to generate your password! \n(Please choose between 8 to 128 characters)");
+
   if (passwordLength <= 8 ||
-    passwordLength >= 128 || passwordLength !== typeof "number") {
-    //Alert user if not a number. 
-      alert("Please choose a number between 8 and 128");
-      //return "" returns an empty string if number is not a number or not the right length
+    passwordLength >= 128 ) {
+      //Alert user if not a number. 
+      alert("Please try again and choose an appropriate password length.")
       return "";
-    }
+  }
+
+  else{
+    alert("Password Length is ok");
+  }
 
   var hasSpecialCharacters = confirm("Would you like to include special characters? i.e. *,&,#,>");
   //user prompt: special characters
@@ -59,8 +63,9 @@ function generatePassword() {
     characterSelectionPool = characterSelectionPool.concat(numbers);
   }
 
-  //This will only run if the user answers Yes to one of the questions above. Otherwise they are returned to the beginning.
 
+  //This will only run if the user answers Yes to one of the questions above. Otherwise they are returned to the beginning.
+  
   if (characterSelectionPool.length > 0) {
     //The for-loop below enables this until i (index choice from characterSelectionPool) is less than the password length requested. If a user chooses 10, this will run 10 times. 
 
@@ -70,27 +75,30 @@ function generatePassword() {
       //passwordText is # of characters in passwordLength (specified by the user in the beginning of the function.) 
       passwordText += characterSelectionPool[
         Math.floor((Math.random()) * characterSelectionPool.length)
-      ]  
+      ]
+      //Console.log shows each character being added to the string from 1-10!  
     }
   }
-
-  else {
-    return "You didn't select any password options - please try again"
+  else{
+    alert("Invalid Password options selected")
   }
-//writes the passwordText to the output of the generatePassword function where it becomes "password" which is written using pwText to the DOM in the id = password section of HTML.
+
   return passwordText;
+  
 }
+console.log(password)
+console.log(passwordText);
 
 function writePassword() {
   var password = generatePassword();
-  var pwText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-  pwText.value = password;
-  //console.log("Your password is " + password);
+  passwordText.value = password;
+ 
   console.log(pwText);
   console.log(passwordText);
 }
 //Write password to the #password input
 // Add event listener to generate button
-//This starts the whole process of calling the write password function which calls the generatePassword function and returns us a value if everything goes the way it should (pw is a # between 8-128 digits, at least one option selected from numbers, special characters, uppercaseLetters, or lowercaseLetters.)
-generateBtn.addEventListener("click", writePassword)
+generateBtn.addEventListener("click", writePassword);
+
